392cfec4a0b436d9d24ceb03b91d310f521290f8
Alex Gschnitzer <75177215+agschnitzer@users.noreply.github.com>
2025-03-06 11:00:52 +0100

    Add image component (#1)
    Also add some styles to the page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
+  import Image from '$lib/elements/Image.svelte'
   import type { PageProps } from './$types'
 
   let { data }: PageProps = $props()
 </script>
 
-<h2 class="font-medium">{data.title}</h2>
-<p>{data.description}</p>
+<div class="max-w-xl mx-auto p-4 bg-slate-200 rounded-md">
+  <Image {...data.image} class="mb-4 mx-auto"/>
+  <h2 class="font-medium">{data.title}</h2>
+  <p>{data.description}</p>
+</div>
