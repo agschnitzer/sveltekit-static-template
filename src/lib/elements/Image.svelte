3cca63b81917618167a1c8b7e9e206fda4475bd1
Alex Gschnitzer <75177215+agschnitzer@users.noreply.github.com>
2025-03-06 11:00:52 +0100

    Add image component (#1)
    Also add some styles to the page.

---
+++ src/lib/elements/Image.svelte
@@ -0,0 +1,8 @@
+<!-- @component Lazy loading image -->
+<script lang="ts">
+  import type { ImageProps } from '$lib/types/component.type'
+
+  let { class: styles, id, src, alt, width, height, srcset, sizes, loading = 'lazy' }: ImageProps = $props()
+</script>
+
+<img {id} {src} {alt} {width} {height} {loading} {srcset} {sizes} fetchpriority={loading === 'lazy' ? 'low' : 'high'} class={styles}>
